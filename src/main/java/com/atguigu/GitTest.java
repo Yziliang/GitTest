0f5abcb3e5c4fa00a4ded6ba8cadc0246b92bb89
Abner Yao <98528432+Yziliang@users.noreply.github.com>
2022-07-09 13:49:50 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-implicit");
         System.out.println("hhhhhhhh");
         System.out.println("aaaaaa");
+        System.out.println("aaaaaa");
     }
 }
